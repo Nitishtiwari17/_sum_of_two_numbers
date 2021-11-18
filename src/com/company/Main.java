78e4565bc8d278ff815ba86e066e13a22917d9de
Nitish Kumar Tiwari <nitishtiwari1710@gmail.com>
2021-11-19 01:38:54 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// sum of two numbers;
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("the sum of two numbers are "+(a+b));
+    }
+}
